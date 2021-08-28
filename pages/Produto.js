5794804c3365d8f262b7d619d31ae29f77734a0a
Maisa <maisa@MacBook-Pro-de-Maisa.local>
2021-08-28 01:21:33 -0300

    Subindo telas em branco sem campo pesquisa em produto

--- pages/Produto.js
+++ pages/Produto.js
@@ -4,10 +4,7 @@ import { Text, View, Button } from 'react-native';
 
 function Produto({ navigation }) {
   return (
-  <View>
-      <View style={{backgroundColor: '#FAD8A8', height: 55, width: '100%'}}>
-        <Text>Busca aqui</Text>
-      </View>
+    <View>
       <Text>Tela de produto aqui</Text>
       <Button
         title="Ir para Carrinho"
